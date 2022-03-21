1e1d815d91e299415106da999492a67bb51f6302
hoshi141015 <hoshito1015@gmail.com>
2022-03-21 16:59:25 +0900

    modul更新

--- pip_mod.sh
+++ pip_mod.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pip install pandas numpy matplotlib scikit-learn tqdm lightgbm requests beautifulsoup4 urllib3 imblearn jupyterthemes ipywidgets widgetsnbextension jupyter
+pip install pandas numpy matplotlib scikit-learn tqdm lightgbm requests beautifulsoup4 urllib3 imblearn jupyterthemes ipywidgets widgetsnbextension jupyter lxml html5lib
